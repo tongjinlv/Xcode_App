9a9f18ad8126b5b8709a0a2b10b35f5511e8a84a
tongjinlv <179990830@qq.com>
2017-08-26 18:09:10 +0800

    Create Datamark Project

---
+++ Datamark/Datamark/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Datamark
+//
+//  Created by 童金吕 on 2017/8/26.
+//  Copyright © 2017年 童金吕. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
